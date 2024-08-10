09e6196d7e710b7ce7ee5efd2e98274d890ac21e
KarthikJR <karthikjrao69@gmail.com>
2024-08-10 20:24:01 +0530

    refactor: Update page component styling and add dark mode support

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -12,7 +12,7 @@ function page() {
 				<BounceCard className="col-span-12 md:col-span-4">
 					<CardTitle>View Project</CardTitle>
 					<div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-violet-400 to-indigo-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
-						<span className="block text-center font-semibold text-indigo-50">
+						<span className="block text-center font-semibold dark:text-indigo-50 text-indigo-950">
 							FEATURE DEMO HERE
 						</span>
 					</div>
@@ -20,7 +20,7 @@ function page() {
 				<BounceCard className="col-span-12 md:col-span-8">
 					<CardTitle>Do another thing</CardTitle>
 					<div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-amber-400 to-orange-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
-						<span className="block text-center font-semibold text-orange-50">
+						<span className="block text-center font-semibold dark:text-orange-50 text-orange-950">
 							FEATURE DEMO HERE
 						</span>
 					</div>
@@ -30,7 +30,7 @@ function page() {
 				<BounceCard className="col-span-12 md:col-span-8">
 					<CardTitle>And this too</CardTitle>
 					<div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-green-400 to-emerald-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
-						<span className="block text-center font-semibold text-emerald-50">
+						<span className="block text-center font-semibold dark:text-emerald-50 text-emerald-950">
 							FEATURE DEMO HERE
 						</span>
 					</div>
@@ -38,7 +38,7 @@ function page() {
 				<BounceCard className="col-span-12 md:col-span-4">
 					<CardTitle>And finally this</CardTitle>
 					<div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-pink-400 to-red-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
-						<span className="block text-center font-semibold text-red-50">
+						<span className="block text-center font-semibold dark:text-red-50 text-red-950">
 							FEATURE DEMO HERE
 						</span>
 					</div>
@@ -58,7 +58,7 @@ const BounceCard = ({
 	return (
 		<motion.div
 			whileHover={{ scale: 0.95, rotate: "-1deg" }}
-			className={`group relative min-h-[300px] cursor-pointer overflow-hidden rounded-2xl bg-slate-100 p-8 ${className}`}
+			className={`group relative min-h-[300px] cursor-pointer overflow-hidden rounded-2xl dark:bg-slate-100 bg-slate-300 p-8 ${className}`}
 		>
 			{children}
 		</motion.div>
@@ -67,7 +67,9 @@ const BounceCard = ({
 
 const CardTitle = ({ children }: { children: ReactNode }) => {
 	return (
-		<h3 className="mx-auto text-center text-3xl font-semibold">{children}</h3>
+		<h3 className="mx-auto text-center text-3xl font-semibold text-black">
+			{children}
+		</h3>
 	);
 };
 
