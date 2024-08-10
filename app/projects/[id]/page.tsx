10c8724dda7d9b7d84b1774cc9426aa33e75e7eb
KarthikJR <karthikjrao69@gmail.com>
2024-08-10 20:24:20 +0530

    feat: Update project page to fetch project details from API

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,56 +1,55 @@
 "use client";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+
+import { getProject } from "@/app/api/Project";
 
 import Heading from "@/components/custom/Heading";
 import { Kanban } from "@/components/custom/Kanban";
-import { usePathname } from "next/navigation";
 
 const ProjectPage = () => {
 	const router = usePathname();
 
-	const links = [
-		{
-			id: 1,
-			title: "Dashboard",
-			description:
-				"View your dashboard that contains all the information you need to know about your project",
-		},
-		{
-			id: 2,
-			title: "Shopping",
-			description:
-				"View your shopping list and add items to your cart to purchase",
-		},
-		{
-			id: 3,
-			title: "Products",
-			description:
-				"View all the products available for purchase and add them to your cart",
-		},
-		{
-			id: 4,
-			title: "Customers",
-			description:
-				"View all the customers that have purchased items from your store",
-		},
-		{
-			id: 5,
-			title: "Analytics",
-			description:
-				"View the analytics of your store to see how well your store is performing",
-		},
-	];
+	const [linkDetails, setLinkDetails] = useState<{
+		id: string;
+		projectName: string;
+		projectDetails: string;
+	}>({
+		id: "",
+		projectName: "Projects",
+		projectDetails: "Display all your projects here",
+	});
 
 	const projectId = router.split("/")[router.split("/").length - 1];
-	const linkDetails = links.find((link) => link.id.toString() === projectId);
+
+	useEffect(() => {
+		try {
+			getProject(projectId).then((data) => {
+				if (data.error) throw data.error;
+				if (data && data.data) {
+					setLinkDetails({
+						id: data.data.projectId,
+						projectName: data.data.projectName,
+						projectDetails: data.data.projectDetails,
+					});
+				}
+			});
+		} catch (error) {
+			postMessage(
+				"error",
+				(error as Error).message || "Error fetching project"
+			);
+		}
+	}, [projectId]);
 
 	return (
 		<div className="overflow-hidden mx-auto max-w-7xl py-12">
 			<Heading
 				projectId={projectId}
-				heading={linkDetails?.title || "Projects"}
-				description={linkDetails?.description}
+				heading={linkDetails.projectName}
+				description={linkDetails.projectDetails}
 			/>
-			<Kanban />
+			<Kanban projectId={projectId} />
 		</div>
 	);
 };
